0cb1f519434e89098ffbc75f4439bbffb96f2b06
Antonio Fiol-Mahon <afiolmahon@gmail.com>
2015-01-13 14:23:41 -0500

    Updated Motor Controllers to Talon SRX using CAN

--- src/org/usfirst/frc/team1218/robot/RobotMap.java
+++ src/org/usfirst/frc/team1218/robot/RobotMap.java
@@ -6,12 +6,12 @@ package org.usfirst.frc.team1218.robot;
  * floating around.
  */
 public class RobotMap {
-	public static final int[] SM_DRIVE_MOTOR = {0, 2, 4, 6};
-	public static final int[] SM_TURN_MOTOR = {1, 3, 5, 7};
+	public static final int[] SM_DRIVE_MOTOR = {10, 12, 14, 16};
+	public static final int[] SM_TURN_MOTOR = {11, 13, 15, 17};
 	public static final int[] SM_ENCODER_A = {0, 2, 4, 6};
 	public static final int[] SM_ENCODER_B = {1, 3, 5, 7};
 	public static final int[] SM_ZERO = {8, 9, 10, 11};
-
+	
     public static final int JOYSTICK_1 = 0;
     
     public static final int GYRO = 1; //Analog input
