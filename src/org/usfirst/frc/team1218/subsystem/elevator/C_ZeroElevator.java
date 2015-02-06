af52266ad80ff1c74ff2127a427a5a325f490d31
Liam Cook <lcook@sch.org>
2015-02-06 00:23:32 -0500

    switched elevator sensor to encoder and worked on zeroing command

--- src/org/usfirst/frc/team1218/subsystem/elevator/C_ZeroElevator.java
+++ src/org/usfirst/frc/team1218/subsystem/elevator/C_ZeroElevator.java
@@ -1,6 +1,8 @@
-package org.usfirst.frc.team1218.subsystem.swerve.math;
+package org.usfirst.frc.team1218.subsystem.elevator;
 
 import edu.wpi.first.wpilibj.command.Command;
+import src.org.usfirst.frc.team1218.robot.Robot;
+import src.org.usfirst.frc.team1218.elevator.Elevator;
 
 /**
  *
@@ -10,14 +12,28 @@ public class C_ZeroElevator extends Command {
     public C_ZeroElevator() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+    	requires(Robot.chassis);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.elevator.configureElevatorMotorControllers();
+    	Robot.elevator.configureElevatorMotorControllersForSpeedControl();
+    	Robot.elevator.setElevatorSpeed(Robot.elevator.ZERO_SPEED);
+    	setTimeout(7);
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	if (Robot.elevator.liftMaster.getFaultRevLim()) {
+    		//if bottom limit switch is hit
+    		Robot.elevator.setElevatorSpeed(0.0);
+    		Robot.elevator.liftMaster.setPosition(0);
+    	}
+    	if(isTimedOut()) {
+    		System.out.println("Error: elevator has not hit limit after running for 7 seconds");
+    	}
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/org/usfirst/frc/team1218/subsystem/elevator/Elevator.java
+++ src/org/usfirst/frc/team1218/subsystem/elevator/Elevator.java
@@ -21,11 +21,16 @@ public class Elevator extends Subsystem {
 	private static final double ELEVATOR_I = 0.0;
 	private static final double ELEVATOR_D = 0.0;
 	
-	public static final int ELEVATOR_DROP_POSITION = 200;
-	public static final int ELEVATOR_CAPTURE_POSITION = 250; //Minimum height needed to get tote in brushes
-	public static final int ELEVATOR_CLEARNCE_POSITION = 650;
-	public static final int ELEVATOR_RAISE_POSITION = 700;
-	public static final int ELEVATOR_STEP_POSITION = 500;
+	public static final int ELEVATOR_SOFT_LIMIT_BOTTOM = 200;
+	public static final int ELEVATOR_DROP_POSITION = 250;
+	public static final int ELEVATOR_CAPTURE_POSITION = 500; //Minimum height needed to get tote in brushes
+	public static final int ELEVATOR_STEP_POSITION = 3000;
+	public static final int ELEVATOR_CLEARNCE_POSITION = 4000;
+	public static final int ELEVATOR_RAISE_POSITION = 5000;
+	public static final int ELEVATOR_SOFT_LIMIT_TOP = 5500;
+
+	public static final double ZERO_SPEED = -0.2;
+	
 	
     public void initDefaultCommand() {
         setDefaultCommand(new C_ElevatorDefault());
@@ -54,9 +59,15 @@ public class Elevator extends Subsystem {
     private void configureElevatorMotorControllers() {
     	liftMaster.enableBrakeMode(true);
     	liftSlave.enableBrakeMode(true);
+    	
     	liftMaster.enableLimitSwitch(true, true);
     	liftMaster.ConfigFwdLimitSwitchNormallyOpen(false);
     	liftMaster.ConfigRevLimitSwitchNormallyOpen(false);
+    	liftMaster.setForwardSoftLimit(ELEVATOR_SOFT_LIMIT_TOP);
+    	liftMaster.enableForwardSoftLimit(true);
+    	liftMaster.setReverseSoftLimit(ELEVATOR_SOFT_LIMIT_BOTTOM);
+    	liftMaster.enableReverseSoftLimit(true);
+    	
     	liftSlave.changeControlMode(CANTalon.ControlMode.Follower);
     	liftSlave.reverseOutput(true);
     	liftSlave.set(liftMaster.getDeviceID());
@@ -64,17 +75,21 @@ public class Elevator extends Subsystem {
     
    int getPosition() {
 	   //TODO: check this get position
-	   return liftMaster.getAnalogInPosition();
+	   return liftMaster.getEncPosition();
    }
     
     void configureElevatorMotorControllersForPID() {
-    	liftMaster.setFeedbackDevice(CANTalon.FeedbackDevice.AnalogEncoder);
+    	liftMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
     	liftMaster.changeControlMode(CANTalon.ControlMode.Position);
     	liftMaster.setPID(ELEVATOR_P, ELEVATOR_I, ELEVATOR_D);
     }
     
     void configureElevatorMotorControllersForSpeedControl() {
+    	//should work with broken encoder
     	liftMaster.changeControlMode(CANTalon.ControlMode.Speed);
+    	liftMaster.enableLimitSwitch(true, true);
+    	liftMaster.enableForwardSoftLimit(false);
+    	liftMaster.enableReverseSoftLimit(false);
     }
     
     public boolean getHasTote() {
@@ -86,3 +101,4 @@ public class Elevator extends Subsystem {
     	//SmartDashboard.putNumber("Intake_Power", intakeL.getSetpoint());
     }
 }
+
