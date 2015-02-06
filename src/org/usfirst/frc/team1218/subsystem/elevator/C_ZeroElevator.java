89aac744fd2e9f1faf5aadb571bc7b7edd23294e
Liam Cook <lcook@sch.org>
2015-02-06 00:26:31 -0500

    minor changes

--- src/org/usfirst/frc/team1218/subsystem/elevator/C_ZeroElevator.java
+++ src/org/usfirst/frc/team1218/subsystem/elevator/C_ZeroElevator.java
@@ -12,7 +12,7 @@ public class C_ZeroElevator extends Command {
     public C_ZeroElevator() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.chassis);
+    	requires(Robot.elevator);
     }
 
     // Called just before this Command runs the first time
@@ -25,20 +25,24 @@ public class C_ZeroElevator extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if (Robot.elevator.liftMaster.getFaultRevLim()) {
-    		//if bottom limit switch is hit
-    		Robot.elevator.setElevatorSpeed(0.0);
-    		Robot.elevator.liftMaster.setPosition(0);
-    	}
-    	if(isTimedOut()) {
-    		System.out.println("Error: elevator has not hit limit after running for 7 seconds");
-    	}
+    	
     	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+    	if (Robot.elevator.liftMaster.getFaultRevLim()) {
+    		//if bottom limit switch is hit
+    		Robot.elevator.setElevatorSpeed(0.0);
+    		Robot.elevator.liftMaster.setPosition(0);
+    		return true;
+    	}
+    	if(isTimedOut()) {
+    		System.out.println("Error: elevator has not hit limit after running for 7 seconds");
+    		Robot.elevator.setElevatorSpeed(0.0);
+    		return true;
+    	}
+    	return false;
     }
 
     // Called once after isFinished returns true
