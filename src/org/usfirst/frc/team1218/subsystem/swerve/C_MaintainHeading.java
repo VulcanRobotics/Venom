b273e08d0fe350f3445d2ed70a5235db33cb6284
afiolmahon <afiolmahon@gmail.com>
2015-02-17 10:24:58 -0500

    WIP added heading maintenence to swerve as toggle not external command beginning other functions unstable committ

--- src/org/usfirst/frc/team1218/subsystem/swerve/C_MaintainHeading.java
+++ src/org/usfirst/frc/team1218/subsystem/swerve/C_MaintainHeading.java
@@ -1,11 +1,7 @@
 package org.usfirst.frc.team1218.subsystem.swerve;
 
-import org.usfirst.frc.team1218.robot.OI;
 import org.usfirst.frc.team1218.robot.Robot;
 
-import edu.wpi.first.wpilibj.PIDController;
-import edu.wpi.first.wpilibj.PIDOutput;
-import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -13,32 +9,16 @@ import edu.wpi.first.wpilibj.command.Command;
  * @author afiol-mahon
  *
  */
-public class C_MaintainHeading extends Command implements PIDOutput, PIDSource{//FIXME Tune PID
+public class C_MaintainHeading extends Command {
 	
-	private PIDController headingController;
 	
 	public C_MaintainHeading() {
 		requires(Robot.swerveDrive);
 	}
 	
-	private static final double HEADING_CONTROLLER_P = 0.03;
-	private static final double HEADING_CONTROLLER_I = 0.0;
-	private static final double HEADING_CONTROLLER_D = 0.0;
-	
 	@Override
 	protected void initialize() {
-		headingController = new PIDController(
-				HEADING_CONTROLLER_P,
-				HEADING_CONTROLLER_I,
-				HEADING_CONTROLLER_D,
-				this,
-				this);
-		headingController.setOutputRange(-1.0, 1.0);
-		headingController.setInputRange(-180, 180);
-		headingController.setContinuous();
-		headingController.enable();
-		headingController.setSetpoint(Robot.swerveDrive.navModule.getYaw());
-		System.out.println("Maintaining Heading of " + headingController.getSetpoint());
+		Robot.swerveDrive.enableHeadingController(Robot.swerveDrive.getHeading());
 	}
 
 	@Override
@@ -52,25 +32,11 @@ public class C_MaintainHeading extends Command implements PIDOutput, PIDSource{/
 
 	@Override
 	protected void end() {
-		headingController.disable();
+		Robot.swerveDrive.disableHeadingController();
 	}
 
 	@Override
 	protected void interrupted() {
 		this.end();
-	}
-	
-	@Override
-	public void pidWrite(double output) {
-		Robot.swerveDrive.powerDrive(
-				OI.getDriverLeftJoystickVector(),
-				output,
-				Robot.swerveDrive.navModule.getYaw()
-				);
-	}
-
-	@Override
-	public double pidGet() {
-		return Robot.swerveDrive.navModule.getYaw();
 	}
 }
