2e2b36055163f2927b208eb8a7f08831ca6d60ba
iMac-2 <afiol-mahon@sch.org>
2015-03-17 11:11:53 -0400

    Fixed Just Drive

--- src/org/usfirst/frc/team1218/commands/auton/Auton_JustDrive.java
+++ src/org/usfirst/frc/team1218/commands/auton/Auton_JustDrive.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team1218.commands.auton;
 import org.usfirst.frc.team1218.commands.swerve.AutoDrive;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *@author afiolmahon
@@ -12,19 +11,6 @@ public class Auton_JustDrive extends CommandGroup {
     
     public  Auton_JustDrive() {
     	addSequential(new Auton_Calibrate());
-    	String direction = SmartDashboard.getString("Calibration_Orientation", "North");
-    	switch (direction) {
-    		default: addSequential(new AutoDrive(10, 0, 0, 1.0));
-    			break;
-    		case "North": addSequential(new AutoDrive(10, 0, 0, 1.0));
-    			break;
-    		case "South": addSequential(new AutoDrive(10, 180, 0, 1.0));
-    			break;
-    		case "East": addSequential(new AutoDrive(10, 90, 0, 1.0));
-    			break;
-    		case "West":addSequential(new AutoDrive(10, 270, 0, 1.0));
-    			break;
-    	}
-        
+    	addSequential(new AutoDrive(10, 0, 0, 1.0));
     }
 }
